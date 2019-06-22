3ab2d8d1a97a322c7df3e01d10b238a7fc10526c
scott park <scott.park@dignsys.com>
2019-06-22 14:23:01 +0900

    Remove wifi firmware download error log
    Signed-off-by: scott park <scott.park@dignsys.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -586,10 +586,12 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 
 	ret = fw_get_filesystem_firmware(device, fw->priv);
 	if (ret) {
+#ifndef	CONFIG_SOC_IMX7D
 		if (!(opt_flags & FW_OPT_NO_WARN))
 			dev_warn(device,
 				 "Direct firmware load for %s failed with error %d\n",
 				 name, ret);
+#endif
 		ret = firmware_fallback_sysfs(fw, name, device, opt_flags, ret);
 	} else
 		ret = assign_fw(fw, device, opt_flags);
--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -151,8 +151,10 @@ static int brcmf_c_process_clm_blob(struct brcmf_if *ifp)
 
 	err = request_firmware(&clm, clm_name, bus->dev);
 	if (err) {
+#ifndef	CONFIG_SOC_IMX7D
 		brcmf_info("no clm_blob available (err=%d), device may have limited channels available\n",
 			   err);
+#endif
 		return 0;
 	}
 
